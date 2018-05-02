4a2df2fc50a3b6c8db455a7964547ed187de1f3a
andrew lupton <aflupton@gmail.com>
2018-05-02 13:33:04 -0700

    add clean commit

---
+++ BestRestaurants.Tests/ModelTests/BestRestaurantsTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BestRestaurants.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BestRestaurantsApp.Tests
+{
+  [TestClass]
+  public class CuisineTest
+  {
+    [TestMethod]
+    public void ()
+    {
+      //test code here
+    }
+  }
+}
