df9aa6882f59f68f082433752bbfbba6f280cc1c
andrew lupton <aflupton@gmail.com>
2018-05-03 15:51:56 -0700

    add changes

---
+++ BestRestaurants/Models/Restaurants.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurantsApp;
+
+namespace BestRestaurants.Models
+{
+  public class Restaurants
+  {
+    //code here
+  }
+}
